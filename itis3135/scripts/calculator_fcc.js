c4a7118cfdeb7b4b634970345de5f21c8970937a
gscar2018 <gscardo@ymail.com>
2023-10-22 22:03:51 -0400

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -14,6 +14,50 @@ const getKeyType = key =>{
     return action
 }
 
+const createResultString = (key, displayedNum, state) =>{
+    const keyContent = key.textContent
+    const keyType = getKeyType(key)
+    const { 
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType
+    } = state
+
+    if (keyType === 'number'){
+      return displayedNum === '0' || 
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+          ? keyContent
+          : displayedNum + keyContent
+    }
+
+    if (keyType === 'decimal'){
+        if (!displayedNum.includes('.')) return displayedNum + '.'
+        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
+        return displayedNum
+    }
+
+    if (keyType === 'operator'){
+        return firstValue && 
+            operator && 
+            previousKeyType !== 'operator' && 
+            previousKeyType !== 'calculate'
+            ? calculate(firstValue, operator, displayedNum)
+            : displayedNum
+    }
+
+    if (keyType === 'clear') return 0
+
+    if (keyType === 'calculate'){
+      return firstValue
+          ? previousKeyType === 'calculate'
+              ? calculate(displayedNum, operator, modValue)
+              : calculate(firstValue, operator, displayedNum)
+          : displayedNum
+    }
+}
+
 const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) =>{
     const keyType = getKeyType(key)
     const {
@@ -64,49 +108,6 @@ const updateVisualState = (key, calculator) =>{
     }
 }
 
-const createResultString = (key, displayedNum, state) =>{
-    const keyContent = key.textContent
-    const keyType = getKeyType(key)
-    const { 
-        firstValue,
-        operator,
-        modValue,
-        previousKeyType
-    } = state
-
-    if (keyType === 'number'){
-      return displayedNum === '0' || 
-        previousKeyType === 'operator' ||
-        previousKeyType === 'calculate'
-          ? keyContent
-          : displayedNum + keyContent
-    }
-
-    if (keyType === 'decimal'){
-        if (!displayedNum.includes('.')) return displayedNum + '.'
-        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
-        return displayedNum
-    }
-
-    if (keyType === 'operator'){
-        return firstValue && 
-            operator && 
-            previousKeyType !== 'operator' && 
-            previousKeyType !== 'calculate'
-            ? calculate(firstValue, operator, displayedNum)
-            : displayedNum
-    }
-
-    if (keyType === 'clear') return 0
-
-    if (keyType === 'calculate'){
-      return firstValue
-          ? previousKeyType === 'calculate'
-              ? calculate(displayedNum, operator, modValue)
-              : calculate(firstValue, operator, displayedNum)
-          : displayedNum
-    }
-}
 
 keys.addEventListener('click', e => {
     if (!e.target.matches('button')) return 
