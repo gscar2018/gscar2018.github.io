1432bda6cca4d5376082d384e5febdfa6fd71989
gscar2018 <gscardo@ymail.com>
2023-11-08 19:48:36 -0500

    Feat: added fizzbuzz

---
+++ itis3135/scripts/fizzbuzz.js
@@ -0,0 +1,33 @@
+let fizzBuzzParagraph = document.getElementById("fizzcontent");
+
+
+let word1 ="Fizz";
+let word2 = "Buzz";
+let word3 = "BANG!";
+
+function fizzBuzzBang(){
+
+    for(let i = 1; i <= 100; i++){
+        let text = "";
+
+        if(i % 3 === 0 ){
+           text += word1;
+        }
+
+        if (i % 5 === 0){
+            text += word2;
+        }
+
+        if (i % 7 === 0 ){
+            text += word3;
+
+        }
+
+        if(text === ""){
+            text += i;
+        }
+        fizzBuzzParagraph.textContent += (text + ", ");
+    } // end for 
+}
+
+fizzBuzzBang()
